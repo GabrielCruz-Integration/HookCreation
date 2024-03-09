7eea22124d7be3abefe56b93fe7f3e563c0edb48
Gabriel Cruz <gmcruz@integrationconsulting.com>
2024-03-08 22:53:31 -0300

    Router OK + styledcomp OK

--- src/pages/Homepage/index.tsx
+++ src/pages/Homepage/index.tsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom'
+import { Container, InsideContainer, SideBar } from './styles'
+
 export default function Homepage() {
   return (
     <>
-      <div>
-        <h1>Homepage</h1>
-      </div>
+      <Container>
+        <SideBar>
+          <h1>Sidebar</h1>
+        </SideBar>
+        <InsideContainer>
+          <h1> Homepage </h1>
+          <Link to="app">Go to app !</Link>
+        </InsideContainer>
+      </Container>
     </>
   )
 }
---
+++ src/pages/Homepage/styles.tsx
@@ -0,0 +1,22 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  display: flex;
+  align-items: start;
+  height: 100vh;
+  width: 100vw;
+
+  margin: 0;
+`
+
+export const SideBar = styled.div`
+  flex: 2;
+  background-color: '#00000';
+  height: 100%;
+  width: 100%;
+`
+export const InsideContainer = styled.div`
+  flex: 8;
+  background-color: '#FFFFF';
+  height: 100vh;
+`
